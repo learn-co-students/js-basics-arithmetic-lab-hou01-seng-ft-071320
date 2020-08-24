fe10af002c0ef3f8575f7764b117ac818cec9524
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2020-08-24 14:25:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here
+let var1 = 2
+let var2 = 31
+const multiply = var1 * var2
+
+let number = 1
+
+const random = Math.random();
+
+const mod = var1 + 2 
+
+const max = Math.max(1, 20)
+
+
+
+
+
+
